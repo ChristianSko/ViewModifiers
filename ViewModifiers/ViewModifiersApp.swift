610b4022ad96ea3cedac689c19de2873610a0852
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-02 09:24:51 +0200

    Initial Commit

---
+++ ViewModifiers/ViewModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifiersApp.swift
+//  ViewModifiers
+//
+//  Created by Skorobogatow, Christian on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
